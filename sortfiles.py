ca1bf4458bb5375851f23a3f85c29c6d5a37540c
nullify0000 <66390375+nullify0000@users.noreply.github.com>
2023-01-19 20:28:36 +0000

    Add files via upload

---
+++ sortfiles.py
@@ -0,0 +1,28 @@
+import easygui
+import os
+import shutil
+# open a directory dialog
+path = easygui.diropenbox()
+# check if pressed cancel
+if path == None: exit()
+# dictionary for filetypes
+satisfied_filetypes = [ ]
+for file in os.listdir(path):
+    # skip directories
+    if os.path.isdir(file):
+        continue
+    index = file.rfind('.')
+    if index < 0:
+        continue
+    extension = file[index:]
+    # check if extension is long enough
+    if len(extension) < len('.js'): 
+        continue
+    # check if we've already created a folder
+    if extension not in satisfied_filetypes:
+        satisfied_filetypes.append(extension)
+        # create a new folder for the extension
+        try: os.makedirs(path + f"\\{extension[1:]}")
+        except: pass
+    # finally, move the file over to the new directory
+    shutil.move(path + f"\\{file}", path + f"\\{extension[1:]}\\{file}")
